ab5571084f5c61e7a70ea14d857353ae3a9ff7db
Liza <elizavetashevchenko135@gmail.com>
2022-07-13 11:31:47 +0300

    xo
    xo

---
+++ src/components/Game.jsx
@@ -0,0 +1,34 @@
+import React, {useState} from 'react'
+import Board from './Board'
+import './Game.css'
+import{calculateWinner} from './log'
+const Game = () => {
+    const [board, setBoard]=useState(Array(9).fill(null))
+    const [xIsNext,setXIsNext]=useState(true)
+    const winner=calculateWinner(board)
+const handleClick=(index)=>{
+    const boardCopy=[...board]
+     
+    if(winner || boardCopy[index]) return
+    boardCopy [index]=xIsNext ? 'X' : 'O'
+    setBoard(boardCopy)
+    setXIsNext(!xIsNext)
+}
+
+const startNewGame=()=>{
+    return(
+        <button className='start-btn' onClick={()=>setBoard(Array(9).fill(null))}>Очистить </button>
+    )
+}
+
+    return (
+        <div className="wrapper">
+           {startNewGame()}
+          <Board squares={board} click={handleClick}/>
+          <p className='game-info'>
+            {winner ? 'Победитель'  + winner : 'Сейчас ходит' + (xIsNext ? 'X' : 'O')}
+          </p>
+</div>
+    );
+} 
+export default Game
